eae1d62e82c2164fc2b935d54f69a13d43fc92fa
Angel Torres <jatorresdev@gmail.com>
2016-12-02 19:00:44 -0500

    Se crea modelo Note y se mapean los campos con respecto a Azure Mobile Client SDK

---
+++ DemosNotas/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace DemosNotas
+{
+	[DataTable("Notes")]
+	public class Note
+	{
+		public string Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Description { get; set; }
+
+		[Version]
+		public string AzureVersion { get; set; }
+
+	}
+}
